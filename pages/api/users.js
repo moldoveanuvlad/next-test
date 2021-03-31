e874ea55b5da70295a0d8002f8c46b8ac870bda6
Vlad Moldoveanu <vmoldoveanu@deloittece.com>
2021-04-01 01:22:50 +0300

    added api route

---
+++ pages/api/users.js
@@ -0,0 +1,41 @@
+import fs from 'fs'
+import csv from 'csv-parser'
+import { data } from 'autoprefixer'
+
+export default (req, res) => {
+  console.log(req.headers)
+  console.log(req.query)
+  console.log(req.cookies)
+
+  const { method } = req
+
+  // access env variables
+  console.log(process.env.API_HOST)
+  // EX 1
+  // allow only POST METHOD EXAMPLE
+  // if (method === 'GET') {
+  //   res.status(501).json({ error: 'not implemented' })
+  // } else {
+  //   res.status(200).json({ name: 'John Doe', ...req.query, method })
+  // }
+
+  // EX 2s
+  // read a csv file
+  if (method === 'GET') {
+    res.status(501).json({ error: 'not implemented' })
+  } else {
+    const csvStream = fs.createReadStream('./pages/api/mockdata.csv')
+      .pipe(csv())
+      .on('data', (row) => {
+        console.log(row.id, req.body.userID)
+        if (row.id === req.body.userID.toString()) {
+          res.status(200).json({ name: `${row.first_name} ${row.last_name}`, email: row.email, gender: row.gender })
+          csvStream.destroy()
+        }
+      })
+      .on('end', () => {
+        res.status(200).json({ response: 'User not found' })
+        console.log('CSV file successfully processed')
+      })
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -4,73 +4,72 @@ import { useState, useEffect } from 'react'
 
 // static generation
 export const getStaticProps = props => {
-  //always the same classroom number
-  const classRoomNumber = Math.floor(Math.random()*10)
-    const userResponse = [{
-      "id": "5tVxgsqPCjv2Ul5Rc7gw",
-      "email": "abigail.liu@example.com",
-      "lastName": "Liu",
-      "title": "miss",
-      "picture": "https://randomuser.me/api/portraits/women/83.jpg",
-      "firstName": "Abigail"
+  // always the same classroom number
+  const classRoomNumber = Math.floor(Math.random() * 10)
+  const userResponse = [{
+    id: '5tVxgsqPCjv2Ul5Rc7gw',
+    email: 'abigail.liu@example.com',
+    lastName: 'Liu',
+    title: 'miss',
+    picture: 'https://randomuser.me/api/portraits/women/83.jpg',
+    firstName: 'Abigail'
   },
   {
-      "id": "6wy6UNkZueJfIUfq88d5",
-      "picture": "https://randomuser.me/api/portraits/women/32.jpg",
-      "firstName": "Melanie",
-      "email": "melanie.pilz@example.com",
-      "title": "miss",
-      "lastName": "Pilz"
+    id: '6wy6UNkZueJfIUfq88d5',
+    picture: 'https://randomuser.me/api/portraits/women/32.jpg',
+    firstName: 'Melanie',
+    email: 'melanie.pilz@example.com',
+    title: 'miss',
+    lastName: 'Pilz'
   },
   {
-      "id": "7DbXNPWlNDR4QYVvFZjr",
-      "email": "evan.carlson@example.com",
-      "firstName": "Evan",
-      "picture": "https://randomuser.me/api/portraits/men/80.jpg",
-      "lastName": "Carlson",
-      "title": "mr"
+    id: '7DbXNPWlNDR4QYVvFZjr',
+    email: 'evan.carlson@example.com',
+    firstName: 'Evan',
+    picture: 'https://randomuser.me/api/portraits/men/80.jpg',
+    lastName: 'Carlson',
+    title: 'mr'
   }]
 
   return {
-    props: {users: userResponse, classRoomNumber}
+    props: { users: userResponse, classRoomNumber }
   }
 }
 
 // server side rendering
 // export const getServerSideProps = props => {
-  //always a different classroom number
-  // const classRoomNumber = Math.floor(Math.random()*10)
-  //   const userResponse = [{
-  //     "id": "5tVxgsqPCjv2Ul5Rc7gw",
-  //     "email": "abigail.liu@example.com",
-  //     "lastName": "Liu",
-  //     "title": "miss",
-  //     "picture": "https://randomuser.me/api/portraits/women/83.jpg",
-  //     "firstName": "Abigail"
-  // },
-  // {
-  //     "id": "6wy6UNkZueJfIUfq88d5",
-  //     "picture": "https://randomuser.me/api/portraits/women/32.jpg",
-  //     "firstName": "Melanie",
-  //     "email": "melanie.pilz@example.com",
-  //     "title": "miss",
-  //     "lastName": "Pilz"
-  // },
-  // {
-  //     "id": "7DbXNPWlNDR4QYVvFZjr",
-  //     "email": "evan.carlson@example.com",
-  //     "firstName": "Evan",
-  //     "picture": "https://randomuser.me/api/portraits/men/80.jpg",
-  //     "lastName": "Carlson",
-  //     "title": "mr"
-  // }]
+// always a different classroom number
+// const classRoomNumber = Math.floor(Math.random()*10)
+//   const userResponse = [{
+//     "id": "5tVxgsqPCjv2Ul5Rc7gw",
+//     "email": "abigail.liu@example.com",
+//     "lastName": "Liu",
+//     "title": "miss",
+//     "picture": "https://randomuser.me/api/portraits/women/83.jpg",
+//     "firstName": "Abigail"
+// },
+// {
+//     "id": "6wy6UNkZueJfIUfq88d5",
+//     "picture": "https://randomuser.me/api/portraits/women/32.jpg",
+//     "firstName": "Melanie",
+//     "email": "melanie.pilz@example.com",
+//     "title": "miss",
+//     "lastName": "Pilz"
+// },
+// {
+//     "id": "7DbXNPWlNDR4QYVvFZjr",
+//     "email": "evan.carlson@example.com",
+//     "firstName": "Evan",
+//     "picture": "https://randomuser.me/api/portraits/men/80.jpg",
+//     "lastName": "Carlson",
+//     "title": "mr"
+// }]
 
-  // return {
-  //   props: {users: userResponse}
-  // }
+// return {
+//   props: {users: userResponse}
 // }
-export default function Home({users, classRoomNumber}) {
-
+// }
+export default function Home ({ users, classRoomNumber }) {
   return (
     <>
       <div className='container'>
@@ -81,11 +80,11 @@ export default function Home({users, classRoomNumber}) {
       <div className='container'>
         <p>Please go to class {classRoomNumber}</p>
         <ul>
-          {users && users.map( user => (
+          {users && users.map(user => (
             <li key={user.id}>
               <h3>{user.title} {user.firstName} {user.lastName}</h3>
               <p>{user.email}</p>
-              <img src={user.picture}/> 
+              <img src={user.picture} />
             </li>)
           )}
         </ul>
